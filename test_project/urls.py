fcecf56717308646b3b2249015897344fc822a55
robertgsinger <robertgsinger@gmail.com>
2019-09-18 08:50:14 -0500

    Divides path and nonpath params from getgo

--- test_project/urls.py
+++ test_project/urls.py
@@ -3,4 +3,4 @@ from rest_framework import routers
 from test_project.testapp.views import get_logs
 
 
-urlpatterns = [url(r"^logs/(?P<id>[0-9])", get_logs, name="logs")]
+urlpatterns = [url(r"^logs/(?P<id>[0-9])/", get_logs, name="logs")]
